1bd0dea382ae1f184c424faaec1ea54a26bef7be
Vladimir Samojlov <v-moi@ya.ru>
2016-12-02 20:00:14 +0200

    add mac and smm

--- app/assets/javascripts/smm/smmCtrl.js
+++ app/assets/javascripts/smm/smmCtrl.js
@@ -5,4 +5,80 @@ angular.module('MyStore').controller('smmCtrl',
 
 function smmCtrl($scope, $state, $http, $localStorage, $sessionStorage, Auth, $stateParams) {	
   console.log('smmCtrl')
+
+  $scope.sections = [
+    {
+    	name: 'Сайт-визитка',
+	    slogan: 'Качественный сайт - успешный бизнес!',
+	    seotitle: 'Заказать сайт-визитку под ключ',
+	    img: 'http://imprimatur.artlebedev.ru/f/1/features/geo-vbank.jpg',
+	    include: [
+	    	{i: 'Уникальный дизайн'},
+		    {i: 'Внутренняя seo-оптимизация'}
+	    ],
+	    seotext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto ipsam nesciunt suscipit ut nemo quidem.',
+	    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti necessitatibus impedit saepe, repellendus reprehenderit voluptatibus aliquid architecto. Libero possimus quidem, alias, ipsam molestiae aliquam, voluptates error, enim debitis molestias exercitationem.'
+  	},
+    {
+    	name: 'Промо-сайт',
+  		slogan: 'Заявите о себе - начните продавать в интернете!',
+	    seotitle: 'Заказать промо-сайт прямо сейчас',
+	    img: 'http://imprimatur.artlebedev.ru/f/1/features/geo-vbank.jpg',
+	    include: [
+	    	{i: 'Уникальный дизайн'},
+		    {i: 'Внутренняя seo-оптимизация'},
+		    {i: 'Техническая консультация'},
+		    {i: 'Бесплатный хостинг на 3 месяца'},
+		    {i: '20% скидка на продвижение'}
+	    ],
+	    seotext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto ipsam nesciunt suscipit ut nemo quidem.',
+	    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti necessitatibus impedit saepe, repellendus reprehenderit voluptatibus aliquid architecto. Libero possimus quidem, alias, ipsam molestiae aliquam, voluptates error, enim debitis molestias exercitationem.'
+  	
+  	},
+    {
+    	name: 'Landing Page',
+  		slogan: 'Универсальное и эффективное решение!',
+	    seotitle: 'Заказать Landing Page',
+	    img: 'http://imprimatur.artlebedev.ru/f/1/features/geo-vbank.jpg',
+	    include: [
+	    	{i: 'Уникальный дизайн'},
+		    {i: 'Индивидуальный подход'}
+	    ],
+	    seotext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto ipsam nesciunt suscipit ut nemo quidem.',
+	    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti necessitatibus impedit saepe, repellendus reprehenderit voluptatibus aliquid architecto. Libero possimus quidem, alias, ipsam molestiae aliquam, voluptates error, enim debitis molestias exercitationem.'
+  	},
+    {
+    	name: 'Корпоративный',
+  		slogan: 'Качественный сайт - успешный бизнес!',
+	    seotitle: 'Заказать сайт-визитку под ключ',
+	    img: 'http://imprimatur.artlebedev.ru/f/1/features/geo-vbank.jpg',
+	    include: [
+	    	{i: 'Уникальный дизайн'},
+		    {i: 'Внутренняя seo-оптимизация'}
+	    ],
+	    seotext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto ipsam nesciunt suscipit ut nemo quidem.',
+	    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti necessitatibus impedit saepe, repellendus reprehenderit voluptatibus aliquid architecto. Libero possimus quidem, alias, ipsam molestiae aliquam, voluptates error, enim debitis molestias exercitationem.'
+  	},
+    {
+    	name: 'Интернет-магазин',
+  		slogan: 'Качественный сайт - успешный бизнес!',
+	    seotitle: 'Заказать сайт-визитку под ключ',
+	    img: 'http://imprimatur.artlebedev.ru/f/1/features/geo-vbank.jpg',
+	    include: [
+	    	{i: 'Уникальный дизайн'},
+		    {i: 'Внутренняя seo-оптимизация'}
+	    ],
+	    seotext: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto ipsam nesciunt suscipit ut nemo quidem.',
+	    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti necessitatibus impedit saepe, repellendus reprehenderit voluptatibus aliquid architecto. Libero possimus quidem, alias, ipsam molestiae aliquam, voluptates error, enim debitis molestias exercitationem.'
+  	},
+  ];
+
+  $scope.selected = $scope.sections[0];
+  $scope.setMaster = function(section) {
+    $scope.selected = section;
+  }
+
+  $scope.isSelected = function(section) {
+    return $scope.selected === section;
+  }
 }
